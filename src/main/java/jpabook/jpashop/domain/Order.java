0522a40dffc1418c28e53ea043451b4a951a8d43
jghan <gojkhan13@gmail.com>
2023-05-29 17:36:52 +0900

    섹션5 마무리

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,8 +19,19 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+    //가급적이면 단반향으로 하자, 필요시에만 양방향으로(복잡도때문에)
+    @ManyToOne //연관관계의 주인
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    //주문서에 연관된 아이템 목록 조회시 양방향 연관관계
+    @OneToMany(mappedBy = "item")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 
     private LocalDateTime orderDate;
 
